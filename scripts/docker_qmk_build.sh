af44a94f18f368411da1c012cadd6be27c7fcd5f
luc wastiaux <luc.wastiaux@airpost.net>
2022-11-26 10:11:10 +0800

    change firmware output location

--- scripts/docker_qmk_build.sh
+++ scripts/docker_qmk_build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+BASEDIR=$(dirname $0)
+python3 $BASEDIR/process_mapping.py
+
 docker run -it \
 --mount type=bind,source="$(pwd)"/keymap,target=/qmk_firmware/keyboards/crkbd/keymaps/luc \
 --mount type=bind,source="$(pwd)"/user,target=/qmk_firmware/users/luc \
---mount type=bind,source="$(pwd)"/firmware,target=/firmware \
+--mount type=bind,source=${HOME}/keyboard/firmware,target=/firmware \
 lucwastiaux/qmk-crkbd-vscode-builder:latest \
 sh -c "cd /qmk_firmware && qmk compile -kb crkbd -km luc && cp *.hex /firmware"
