41bd37951beaa1f2ca76ddbc9de8c96d14285389
luc wastiaux <luc.wastiaux@airpost.net>
2022-12-16 14:23:24 +0800

    don't have standalone setup script

--- scripts/docker_qmk_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. qmk_tag.sh
-
-echo "QMK_TAG: " $QMK_TAG
-docker build --build-arg QMK_TAG=$QMK_TAG --build-arg UNAME=$(id -un) --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t lucwastiaux/qmk-crkbd-vscode-builder:QMK-${QMK_TAG} -f Dockerfile .
