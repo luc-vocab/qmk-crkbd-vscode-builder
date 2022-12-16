0dfcd1705f3aab8eee6fda7ff8ca67359c86bfb7
luc <lucw@mailc.net>
2022-12-16 14:43:31 +0800

    tag repo to be able to re-build stable firmware

---
+++ scripts/snap_stable_firmware.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VERSION_NUMBER=$1 # for example 0.1
+GIT_TAG=v${VERSION_NUMBER}
+
+git tag -a ${GIT_TAG} -m "version ${GIT_TAG}"
+git push origin ${GIT_TAG}
+
+echo "qmk-crkbd-vscode-builder tag ${GIT_TAG}" > build_info.txt
+rclone copy ~/keyboard/firmware/*.hex dropbox:Keyboard/stable_firmware
+rclone copy build_info.txt dropbox:Keyboard/stable_firmware
