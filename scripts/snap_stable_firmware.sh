d92f2d5ef64179204018337c5671692b4e5e6daf
luc <lucw@mailc.net>
2022-12-16 14:45:19 +0800

    add hashes

--- scripts/snap_stable_firmware.sh
+++ scripts/snap_stable_firmware.sh
@@ -7,5 +7,6 @@ git tag -a ${GIT_TAG} -m "version ${GIT_TAG}"
 git push origin ${GIT_TAG}
 
 echo "qmk-crkbd-vscode-builder tag ${GIT_TAG}" > build_info.txt
+md5sum ~/keyboard/firmware/*.hex >> build_info.txt
 rclone copy ~/keyboard/firmware/*.hex dropbox:Keyboard/stable_firmware
 rclone copy build_info.txt dropbox:Keyboard/stable_firmware
