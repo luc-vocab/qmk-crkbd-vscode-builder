5654d788b0fb628ed8ff37d794abee0cafdbd1de
Luc W <lucw@airpost.net>
2022-12-17 16:10:17 +0800

    only use left+right shift press to activate caps word.

--- user/config.h
+++ user/config.h
@@ -3,6 +3,19 @@
 
 #pragma once
 
-#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+// oneshot settings
+// ===============
+#define ONESHOT_TIMEOUT 5000
+
+// caps word settings
+// ==================
+// don't want the double tap
+// #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define CAPS_WORD_IDLE_TIMEOUT 5000
+
+// tap settings 
+// ============
+
 #undef TAPPING_TERM
 #define TAPPING_TERM 175
