a89f7743fa0a12fbc208fc81485c2b884f71055f
Luc W <lucw@airpost.net>
2023-12-06 21:59:54 +0800

    add shell shortcuts on the left side

--- user/keymap_appnav.h
+++ user/keymap_appnav.h
@@ -21,20 +21,20 @@
 // APPNAV_SHELL
 // ============
 
-#define __APPNAV_SHELL_L0__  KC_TRNS,            KC_TRNS,           KC_TRNS,          KC_TRNS,           KC_TRNS,          KC_TRNS
-#define __APPNAV_SHELL_L1__  KC_TRNS,            KC_TRNS,           KC_TRNS,          KC_TRNS,           KC_TRNS,          KC_TRNS
-#define __APPNAV_SHELL_L2__  TO(BASE_SHELL),     KC_TRNS,           KC_TRNS,          KC_TRNS,           KC_TRNS,          KC_TRNS
-#define __APPNAV_SHELL_L3__  KC_TRNS,            KC_TRNS,           KC_TRNS,          KC_TRNS,           KC_TRNS,          KC_TRNS
+#define __APPNAV_SHELL_L0__  KC_TRNS,            KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,          KC_TRNS
+#define __APPNAV_SHELL_L1__  KC_TRNS,            KC_TRNS,           SHELL_PGREP,       SHELL_PLESS,       SHELL_LESS,       SH_EXIT
+#define __APPNAV_SHELL_L2__  TO(BASE_SHELL),     KC_TRNS,           SHELL_CDPRE,       SHELL_LSLTR,       SHELL_LS,         SHELL_LSLA
+#define __APPNAV_SHELL_L3__  KC_TRNS,            SHELL_SCREEN_LIST, SHELL_SCREENRD,    SHELL_SCREEN_NEW,  SHELL_TAILF,      KC_TRNS
 
-#define __APPNAV_SHELL_R0__  KC_TRNS,            KC_TRNS,           KC_TRNS,          KC_TRNS,           KC_TRNS,          KC_TRNS
-#define __APPNAV_SHELL_R1__  KC_TRNS,            KC_TRNS,           KC_TRNS,          KC_TRNS,           KC_TRNS,          KC_TRNS
-#define __APPNAV_SHELL_R2__  KC_TRNS,            SCREEN_TAB_LEFT,   SCREEN_TAB_RIGHT, SCREEN_NUMBER,     SCREEN_RENAME,    SH_DEL_WORD
-#define __APPNAV_SHELL_R3__  SH_DEL_TO_START,    SH_RECALL,         SH_SEARCH,        SH_PASTE,          SH_DEL_TO_END,    SH_STOP
+#define __APPNAV_SHELL_R0__  KC_TRNS,            KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,          KC_TRNS
+#define __APPNAV_SHELL_R1__  KC_TRNS,            KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,          KC_TRNS
+#define __APPNAV_SHELL_R2__  KC_TRNS,            SCREEN_TAB_LEFT,   SCREEN_TAB_RIGHT,  SCREEN_NUMBER,     SCREEN_RENAME,    SH_DEL_WORD
+#define __APPNAV_SHELL_R3__  SH_DEL_TO_START,    SH_RECALL,         SH_SEARCH,         SH_PASTE,          SH_DEL_TO_END,    SH_STOP
 
-#define __APPNAV_SHELL_L_2THUMB__  KC_TRNS, KC_TRNS
-#define __APPNAV_SHELL_R_2THUMB__  SCREEN_KILL,        SCREEN_NEW_TAB
-#define __APPNAV_SHELL_L_3THUMB__  KC_TRNS, KC_TRNS, KC_TRNS
-#define __APPNAV_SHELL_R_3THUMB__  SCREEN_KILL,        SCREEN_NEW_TAB,    KC_TRNS
+#define __APPNAV_SHELL_L_2THUMB__  KC_TRNS,     KC_TRNS
+#define __APPNAV_SHELL_R_2THUMB__  SCREEN_KILL, SCREEN_NEW_TAB
+#define __APPNAV_SHELL_L_3THUMB__  KC_TRNS,     KC_TRNS,           KC_TRNS
+#define __APPNAV_SHELL_R_3THUMB__  SCREEN_KILL, SCREEN_NEW_TAB,    KC_TRNS
 
 // APPNAV_VSCODE
 // ============
