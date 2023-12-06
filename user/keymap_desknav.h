1b843f3a47b8955f40cbcd26ca71ddc29c23462a
Luc W <lucw@airpost.net>
2023-12-07 07:32:27 +0800

    no need for base browser, just press desknav twice to reset

--- user/keymap_desknav.h
+++ user/keymap_desknav.h
@@ -8,7 +8,7 @@
 #define __DESKNAV_DEFAULT_R0__  KC_TRNS,          KC_TRNS,          KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS
 #define __DESKNAV_DEFAULT_R1__  MEH(KC_F1),       MEH(KC_F2),       MEH(KC_F3),    MEH(KC_F4),    MEH(KC_F5),    MEH(KC_F6)
 #define __DESKNAV_DEFAULT_R2__  MEH(KC_F7),       OS_WS_1,          OS_WS_2,       OS_WS_3,       OS_WS_4,       MEH(KC_F13)
-#define __DESKNAV_DEFAULT_R3__  KC_TRNS,          OS_WS_LEFT,       OS_WS_RIGHT,   OS_WS_SHOW,    KC_TRNS,       TO(BASE_BROWSER)
+#define __DESKNAV_DEFAULT_R3__  KC_TRNS,          OS_WS_LEFT,       OS_WS_RIGHT,   OS_WS_SHOW,    KC_TRNS,       KC_TRNS
 
 #define __DESKNAV_DEFAULT_L_2THUMB__  KC_TRNS,  KC_TRNS
 #define __DESKNAV_DEFAULT_R_2THUMB__  KC_TRNS,  KC_TRNS
