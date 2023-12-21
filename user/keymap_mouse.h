6bec828b3406cf31ed3676e0fd7525d8197debdd
Luc W <lucw@airpost.net>
2023-12-21 08:45:25 +0800

    fix wheel up / down

--- user/keymap_mouse.h
+++ user/keymap_mouse.h
@@ -3,15 +3,15 @@
 // mouse layer
 // ===========
 
-#define __MOUSE_L0__  KC_TRNS,               KC_TRNS,       KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS
-#define __MOUSE_L1__  OSL(DESKNAV_DEFAULT),  KC_TRNS,       KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS
-#define __MOUSE_L2__  KC_TRNS,               KC_TRNS,       KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS
-#define __MOUSE_L3__  KC_TRNS,               KC_TRNS,       KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS
+#define __MOUSE_L0__  KC_TRNS,               KC_TRNS,       KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS
+#define __MOUSE_L1__  OSL(DESKNAV_DEFAULT),  KC_TRNS,       KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS
+#define __MOUSE_L2__  KC_TRNS,               KC_TRNS,       KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS
+#define __MOUSE_L3__  KC_TRNS,               KC_TRNS,       KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS
 
-#define __MOUSE_R0__  KC_TRNS,               KC_TRNS,       KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS
-#define __MOUSE_R1__  KC_TRNS,               KC_MS_WH_DOWN, KC_MS_UP,   KC_MS_WH_UP, KC_TRNS,    KC_TRNS
-#define __MOUSE_R2__  KC_TRNS,               KC_MS_LEFT,    KC_MS_DOWN, KC_MS_RIGHT, KC_MS_BTN3, KC_TRNS
-#define __MOUSE_R3__  KC_TRNS,               KC_TRNS,       KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS
+#define __MOUSE_R0__  KC_TRNS,               KC_TRNS,       KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS
+#define __MOUSE_R1__  KC_TRNS,               KC_MS_WH_UP,   KC_MS_UP,   KC_MS_WH_DOWN, KC_TRNS,    KC_TRNS
+#define __MOUSE_R2__  KC_TRNS,               KC_MS_LEFT,    KC_MS_DOWN, KC_MS_RIGHT,   KC_MS_BTN3, KC_TRNS
+#define __MOUSE_R3__  KC_TRNS,               KC_TRNS,       KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS
 
 #define __MOUSE_L_2THUMB__ KC_TRNS, KC_TRNS
 #define __MOUSE_R_2THUMB__ KC_MS_BTN2, KC_MS_BTN1
