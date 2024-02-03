38806523c0261b6cf4c5429a2cd926707a4d2eca
Luc W <lucw@airpost.net>
2024-02-03 10:13:52 +0800

    modify game layer

--- user/keymap_game.h
+++ user/keymap_game.h
@@ -1,17 +1,17 @@
 // GAME
 // ============
 
-#define __GAME_L0__  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-#define __GAME_L1__  KC_TAB,    KC_TRNS, KC_Q,    KC_W,    KC_E,    KC_R
-#define __GAME_L2__  KC_ENTER,  KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F
-#define __GAME_L3__  KC_TRNS,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B
+#define __GAME_L0__  KC_TRNS,       KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS
+#define __GAME_L1__  KC_TAB,        KC_T,    KC_Q,     KC_W,    KC_E,     KC_R
+#define __GAME_L2__  KC_ENTER,      KC_LSFT,    KC_A,     KC_S,    KC_D,     KC_F
+#define __GAME_L3__  KC_G,         KC_Z,    KC_X,     KC_C,    KC_V,     KC_B
 
-#define __GAME_R0__  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-#define __GAME_R1__  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_ESC
-#define __GAME_R2__  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-#define __GAME_R3__  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+#define __GAME_R0__  KC_TRNS,       KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS
+#define __GAME_R1__  KC_Y,          KC_U,    KC_I,     KC_O,    KC_P,     KC_ESC
+#define __GAME_R2__  KC_H,          KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT
+#define __GAME_R3__  KC_N,          KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  KC_ESC
 
 #define __GAME_L_2THUMB__  KC_TRNS, KC_TRNS
 #define __GAME_R_2THUMB__  KC_TRNS, KC_TRNS
-#define __GAME_L_3THUMB__  KC_LCTL,  KC_LALT, KC_SPACE
+#define __GAME_L_3THUMB__  KC_TRNS,  KC_LCTL, KC_SPACE
 #define __GAME_R_3THUMB__  KC_TRNS,  KC_TRNS, TO(BASE)
