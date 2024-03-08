711bfd866eacf8873ba5ad509dd0d82564d3c11e
Luc W <lucw@airpost.net>
2024-03-08 09:30:15 +0800

    align text

--- user/keymap_appnav.h
+++ user/keymap_appnav.h
@@ -21,15 +21,15 @@
 // SHELL
 // ============
 
-#define __SHELL_L0__  KC_TRNS,            KC_TRNS,            KC_TRNS,           KC_TRNS,                 KC_TRNS,          KC_TRNS
-#define __SHELL_L1__  SCREEN_PASTE,       KC_TRNS,            SHELL_PGREP,       SHELL_PLESS,             SHELL_LESS,       SH_EXIT
-#define __SHELL_L2__  SCREEN_COPY_MODE,   OSL(SCREEN), SHELL_CDPRE,       SHELL_LSLTR,             SHELL_LS,         SHELL_LSLA
-#define __SHELL_L3__  TO(BASE),           SHELL_SCREEN_LIST,  SHELL_SCREENRD,    SHELL_SCREEN_NEW,        SHELL_TAILF,      SH_CLOSE
+#define __SHELL_L0__  KC_TRNS,            KC_TRNS,            KC_TRNS,           KC_TRNS,          KC_TRNS,          KC_TRNS
+#define __SHELL_L1__  SCREEN_PASTE,       KC_TRNS,            SHELL_PGREP,       SHELL_PLESS,      SHELL_LESS,       SH_EXIT
+#define __SHELL_L2__  SCREEN_COPY_MODE,   OSL(SCREEN),        SHELL_CDPRE,       SHELL_LSLTR,      SHELL_LS,         SHELL_LSLA
+#define __SHELL_L3__  TO(BASE),           SHELL_SCREEN_LIST,  SHELL_SCREENRD,    SHELL_SCREEN_NEW, SHELL_TAILF,      SH_CLOSE
 
-#define __SHELL_R0__  KC_TRNS,            KC_TRNS,            KC_TRNS,           KC_TRNS,                 KC_TRNS,          KC_TRNS
-#define __SHELL_R1__  KC_TRNS,            SCREEN_MOVE_LEFT,   SCREEN_MOVE_RIGHT, SCREEN_RENAME,           SH_CLEARSCR,      SH_DEL_PATH
-#define __SHELL_R2__  SH_WORD_LEFT,       SCREEN_TAB_LEFT,    SCREEN_TAB_RIGHT,  SCREEN_NUMBER,           SH_WORD_RIGHT,    SH_DEL_WORD
-#define __SHELL_R3__  SH_DEL_TO_START,    SH_RECALL,          SH_SEARCH,         SH_PASTE,                SH_DEL_TO_END,    SH_STOP
+#define __SHELL_R0__  KC_TRNS,            KC_TRNS,            KC_TRNS,           KC_TRNS,          KC_TRNS,          KC_TRNS
+#define __SHELL_R1__  KC_TRNS,            SCREEN_MOVE_LEFT,   SCREEN_MOVE_RIGHT, SCREEN_RENAME,    SH_CLEARSCR,      SH_DEL_PATH
+#define __SHELL_R2__  SH_WORD_LEFT,       SCREEN_TAB_LEFT,    SCREEN_TAB_RIGHT,  SCREEN_NUMBER,    SH_WORD_RIGHT,    SH_DEL_WORD
+#define __SHELL_R3__  SH_DEL_TO_START,    SH_RECALL,          SH_SEARCH,         SH_PASTE,         SH_DEL_TO_END,    SH_STOP
 
 #define __SHELL_L_2THUMB__  SHELL_GIT_DIFF,     SHELL_GIT_STATUS
 #define __SHELL_R_2THUMB__  SCREEN_KILL,        SCREEN_NEW_TAB
@@ -41,12 +41,12 @@
 // =============
 #define __SCREEN_L0__  KC_TRNS,          KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,          KC_TRNS
 #define __SCREEN_L1__  KC_TRNS,          KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,          KC_TRNS
-#define __SCREEN_L2__  KC_TRNS,   KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,          KC_TRNS
+#define __SCREEN_L2__  KC_TRNS,          KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,          KC_TRNS
 #define __SCREEN_L3__  KC_TRNS,          KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,          KC_TRNS
 
 #define __SCREEN_R0__  KC_TRNS,          KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,          KC_TRNS
 #define __SCREEN_R1__  KC_TRNS,          SCREEN_7,   SCREEN_8,  SCREEN_9,  SCREEN_RENAME,    KC_TRNS
-#define __SCREEN_R2__  SCREEN_TAB_LEFT,  SCREEN_4,   SCREEN_5,  SCREEN_6,  SCREEN_TAB_RIGHT, SCREEN_COPY_MODE                              
+#define __SCREEN_R2__  SCREEN_TAB_LEFT,  SCREEN_4,   SCREEN_5,  SCREEN_6,  SCREEN_TAB_RIGHT, SCREEN_COPY_MODE
 #define __SCREEN_R3__  KC_TRNS,          SCREEN_1,   SCREEN_2,  SCREEN_3,  SCREEN_NUMBER,    SCREEN_PASTE
 
 #define __SCREEN_L_2THUMB__  KC_TRNS,       KC_TRNS
@@ -58,15 +58,15 @@
 // VSCODE
 // ============
 
-#define __VSCODE_L0__  KC_TRNS,          KC_TRNS,         KC_TRNS,       KC_TRNS,           KC_TRNS,     KC_TRNS
+#define __VSCODE_L0__  KC_TRNS,           KC_TRNS,         KC_TRNS,       KC_TRNS,           KC_TRNS,     KC_TRNS
 #define __VSCODE_L1__  TO(BASE),          VS_GO_TO_TOP,    VS_FIND_FILES, VS_CMT_BLOCK,      VS_CMT_LINE, VS_RECENT
-#define __VSCODE_L2__  KC_TRNS,  KC_TRNS,         KC_TRNS,       KC_TRNS,           KC_TRNS,     VS_SEL_BRACKET
-#define __VSCODE_L3__  KC_TRNS,          VS_GO_TO_BOTTOM, KC_TRNS,       KC_TRNS,           KC_TRNS,     KC_TRNS
+#define __VSCODE_L2__  KC_TRNS,           KC_TRNS,         KC_TRNS,       KC_TRNS,           KC_TRNS,     VS_SEL_BRACKET
+#define __VSCODE_L3__  KC_TRNS,           VS_GO_TO_BOTTOM, KC_TRNS,       KC_TRNS,           KC_TRNS,     KC_TRNS
 
-#define __VSCODE_R0__  KC_TRNS,          KC_TRNS,         KC_TRNS,       KC_TRNS,           KC_TRNS,     KC_TRNS
-#define __VSCODE_R1__  VS_COPYLINEDOWN,  VS_REFERENCES,   VS_DEFINITION, VS_IMPLEMENTATION, VS_LINE,     VS_BRACKET
-#define __VSCODE_R2__  VS_DEL_LINE,      VS_TABLEFT,      VS_TABRIGHT,   VS_SYMBOLEDITOR,   VS_FILE,     VS_BACK
-#define __VSCODE_R3__  VS_BM_CLEARALL,   VS_BM_PREV,      VS_BM_NEXT,    VS_GROUP_1,        VS_GROUP_2,  VS_BM_TOGGLE
+#define __VSCODE_R0__  KC_TRNS,           KC_TRNS,         KC_TRNS,       KC_TRNS,           KC_TRNS,     KC_TRNS
+#define __VSCODE_R1__  VS_COPYLINEDOWN,   VS_REFERENCES,   VS_DEFINITION, VS_IMPLEMENTATION, VS_LINE,     VS_BRACKET
+#define __VSCODE_R2__  VS_DEL_LINE,       VS_TABLEFT,      VS_TABRIGHT,   VS_SYMBOLEDITOR,   VS_FILE,     VS_BACK
+#define __VSCODE_R3__  VS_BM_CLEARALL,    VS_BM_PREV,      VS_BM_NEXT,    VS_GROUP_1,        VS_GROUP_2,  VS_BM_TOGGLE
 
 #define __VSCODE_L_2THUMB__  KC_TRNS,     KC_TRNS
 #define __VSCODE_R_2THUMB__  VS_CLOSETAB, VS_COMMANDS
