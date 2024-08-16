b28faabc93c29f976b9c6e3960c32ec135734b73
Luc W <lucw@airpost.net>
2024-08-17 07:11:52 +0800

    remove unneeded settings

--- keymap_crkbd_v41/config.h
+++ keymap_crkbd_v41/config.h
@@ -14,17 +14,6 @@
 
 #define MOUSEKEY_TIME_TO_MAX 200
 
+// RGB light settings
+// ==================
 #define RGBLIGHT_LAYERS
-
-// #ifdef RGBLIGHT_ENABLE
-//     #define RGBLIGHT_HUE_STEP 10
-//     #define RGBLIGHT_SAT_STEP 17
-//     #define RGBLIGHT_VAL_STEP 17
-// #endif
-
-
-// // we don't want any colors by default
-// #define RGBLIGHT_DEFAULT_HUE 	0
-// #define RGBLIGHT_DEFAULT_SAT 	0
-// #define RGBLIGHT_DEFAULT_VAL    0
-
