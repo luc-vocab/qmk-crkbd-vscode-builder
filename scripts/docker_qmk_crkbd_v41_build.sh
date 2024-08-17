25b8d3db0ca840a84190f0bb93f5ad9db6b5b7c5
Luc W <lucw@airpost.net>
2024-08-17 08:01:03 +0800

    cleanups, using footstand firmware

--- scripts/docker_qmk_crkbd_v41_build.sh
+++ scripts/docker_qmk_crkbd_v41_build.sh
@@ -15,17 +15,17 @@ if [ "$REBUILD_DOCKER" = "rebuild" ]; then
     # build image if not already built (will cache)
     docker build --build-arg QMK_TAG=$QMK_TAG --build-arg UNAME=$(id -un) --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t ${DOCKER_IMAGE} -f Dockerfile.crkbd_v41 .
     # push image to docker hub
-    docker push ${DOCKER_IMAGE}
+    # docker push ${DOCKER_IMAGE}
 fi
 
 
 docker run --rm -it \
---mount type=bind,source="$(pwd)"/keymap_crkbd_v41,target=/workspace/qmk_firmware/keyboards/crkbd/rev4/keymaps/luc \
+--mount type=bind,source="$(pwd)"/keymap_crkbd_v41,target=/workspace/kbd_firmware/keyboards/crkbd/rev4_1/keymaps/luc \
 --mount type=bind,source="$(pwd)"/user,target=/workspace/qmk_firmware/users/luc \
 --mount type=bind,source=${HOME}/keyboard/firmware,target=/firmware \
 --name qmk-crkbd-vscode-builder \
 ${DOCKER_IMAGE} \
-sh -c "cd /workspace/qmk_firmware && qmk compile -kb crkbd/rev4 -km luc && cp *.uf2 /firmware" || exit 1
+sh -c "cd /workspace/kbd_firmware && kb=crkbd kr=rev4_1/standard km=via make qmk-compile && cp *.uf2 /firmware"
 
 rclone copy ~/keyboard/firmware/*.uf2 dropbox:Keyboard/firmware
 rclone copy ~/keyboard/qmk-crkbd-vscode-builder/keybindings.json dropbox:Keyboard/vscode
