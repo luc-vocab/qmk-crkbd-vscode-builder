6fe7b589087a0f89e2a105b0b425192334f518dd
Luc W <lucw@airpost.net>
2024-09-10 06:19:47 +0800

    switch ctrl and browser

--- user/keymap_base.h
+++ user/keymap_base.h
@@ -6,7 +6,7 @@
 
 #define __BASE_L0__  OSL(DESKNAV),  MO(MOUSE),    OSM(MOD_LGUI),   OSM(MOD_LALT),   OSM(MOD_LCTL),   WS_SHOW
 #define __BASE_L1__  OSL(VSCODE),   KC_QUOTE,     KC_COMMA,        KC_DOT,          KC_P,            KC_Y
-#define __BASE_L2__  MO(BROWSER),   KC_A,         KC_O,            KC_E,            KC_U,            KC_I
+#define __BASE_L2__  OSM(MOD_LCTL), KC_A,         KC_O,            KC_E,            KC_U,            KC_I
 #define __BASE_L3__  OSM(MOD_LSFT), OSL(SHELL),   KC_Q,            KC_J,            KC_K,            KC_X
 
 #define __BASE_R0__  WS_LEFT,       MEH(KC_F8),   MEH(KC_F9),      MEH(KC_F10),     MEH(KC_F12),     WS_RIGHT
@@ -21,6 +21,6 @@
 
 #define __BASE_L_2THUMB__  MO(SYMBOLS),       MO(KEYNAV)
 #define __BASE_R_2THUMB__  KC_ENTER,          KC_SPACE
-#define __BASE_L_3THUMB__  OSM(MOD_LCTL),     __BASE_L_2THUMB__
+#define __BASE_L_3THUMB__  MO(BROWSER),       __BASE_L_2THUMB__
 #define __BASE_R_3THUMB__  __BASE_R_2THUMB__, MO(DESKNAV)
 
