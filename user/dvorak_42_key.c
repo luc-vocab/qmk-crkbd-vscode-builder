e2a09009697353e1f4c8b15c99b15a67d2dcece8
Luc W <lucw@airpost.net>
2024-09-10 14:39:01 +0800

    new tmux shortcuts

--- user/dvorak_42_key.c
+++ user/dvorak_42_key.c
@@ -125,6 +125,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         TMUX_SHORTCUT(TMUX_TAB_LEFT, "p");
         TMUX_SHORTCUT(TMUX_TAB_RIGHT, "n");
         TMUX_SHORTCUT(TMUX_LIST_WIN, "w");
+        TMUX_SHORTCUT(TMUX_NEW_WIN,  "c");
+        TMUX_SHORTCUT(TMUX_RENAME_WIN, ",");
+        TMUX_SHORTCUT(TMUX_KILL_WIN, "&");
+        TMUX_SHORTCUT(TMUX_MOVE_WIN_LEFT, "swap-window -t -1");
+        TMUX_SHORTCUT(TMUX_MOVE_WIN_RIGHT, "swap-window -t +1");
 
         TMUX_SHORTCUT(TMUX_WIN_1, "1");
         TMUX_SHORTCUT(TMUX_WIN_2, "2");
