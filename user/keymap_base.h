1ead2ea09d317d930153c640c62a5a48d5aa7d40
Luc W <lucw@airpost.net>
2024-10-02 11:18:44 +0800

    add win key on extra keys of the crkbd v41

--- user/keymap_base.h
+++ user/keymap_base.h
@@ -15,7 +15,7 @@
 #define __BASE_R3__  KC_B,          KC_M,            KC_W,            KC_V,            KC_Z,            OSM(MOD_RSFT)
 
 #define __BASE_L1_EXT__ __BASE_L1__,   KC_TRNS
-#define __BASE_L2_EXT__ __BASE_L2__,   KC_TRNS
+#define __BASE_L2_EXT__ __BASE_L2__,   KC_LGUI
 #define __BASE_R1_EXT__ KC_TRNS,       __BASE_R1__
 #define __BASE_R2_EXT__ OSM(MOD_LALT), __BASE_R2__
 
