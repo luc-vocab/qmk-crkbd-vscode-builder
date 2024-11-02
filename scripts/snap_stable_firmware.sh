e013349bc3ccbca65daace181ad14ddcf2aca996
Luc W <lucw@airpost.net>
2024-11-03 04:32:15 +0800

    copy hierarchy of firmware to stable_firmware

--- scripts/snap_stable_firmware.sh
+++ scripts/snap_stable_firmware.sh
@@ -12,8 +12,6 @@ git tag -a ${GIT_TAG} -m "version ${GIT_TAG}"
 git push origin ${GIT_TAG}
 
 echo "qmk-crkbd-vscode-builder tag ${GIT_TAG}" > build_info.txt
-md5sum ~/keyboard/firmware/*.hex >> build_info.txt
-md5sum ~/keyboard/firmware/*.bin >> build_info.txt
-rclone copy ~/keyboard/firmware/*.hex dropbox:Keyboard/stable_firmware
-rclone copy ~/keyboard/firmware/*.bin dropbox:Keyboard/stable_firmware
+find ~/keyboard/firmware/ -type f | xargs md5sum >> build_info.txt
+rclone sync ~/keyboard/firmware/ dropbox:Keyboard/stable_firmware/
 rclone copy build_info.txt dropbox:Keyboard/stable_firmware
