2de435dbacd58aa65343ff62f91be0f2085c808c
Luc W <lucw@airpost.net>
2025-04-13 07:53:32 +0800

    fix voyager script paths

--- scripts/docker_qmk_zsa_build.sh
+++ scripts/docker_qmk_zsa_build.sh
@@ -3,6 +3,7 @@
 set -xe
 
 BASEDIR=$(dirname $0)
+. $BASEDIR/directories.sh
 python3 $BASEDIR/process_mapping.py
 . $BASEDIR/zsa_qmk_tag.sh
 REBUILD_DOCKER=$1
@@ -21,10 +22,10 @@ fi
 docker run --rm -it \
 --mount type=bind,source="$(pwd)"/keymap_voyager,target=/workspace/qmk_firmware/keyboards/voyager/keymaps/luc \
 --mount type=bind,source="$(pwd)"/user,target=/workspace/qmk_firmware/users/luc \
---mount type=bind,source=${HOME}/code/keyboard/firmware,target=/firmware \
+--mount type=bind,source=${FIRMWARE_DIR},target=/firmware \
 --name qmk-crkbd-vscode-builder \
 ${DOCKER_IMAGE} \
 sh -c "cd /workspace/qmk_firmware && qmk compile -kb voyager -km luc && cp *.bin /firmware/voyager/"
 
-rclone sync ~/keyboard/firmware/voyager/ dropbox:Keyboard/firmware/voyager/
-rclone copy ~/keyboard/qmk-crkbd-vscode-builder/keybindings.json dropbox:Keyboard/vscode
+rclone sync ${FIRMWARE_DIR}/voyager/ dropbox:Keyboard/firmware/voyager/
+rclone copy ${KEYBOARD_DIR}/qmk-crkbd-vscode-builder/keybindings.json dropbox:Keyboard/vscode
